dbe3555afdc324acbe1ea2302aef146a59f74063
Naveen Kumar Bisai <31248446+NaveenkumarBisai@users.noreply.github.com>
2025-08-23 23:12:07 +0530

    Create RotateImage.cpp

---
+++ Practice/RotateImage.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    // Step 1: Transpose the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    rotate(matrix);
+
+    // Print rotated matrix
+    for (auto row : matrix) {
+        for (int val : row) cout << val << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
